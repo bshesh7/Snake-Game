6fc8fc041325c0ebea0bda0d52c57996a50eeb2d
bshesh7 <44710882+bshesh7@users.noreply.github.com>
2019-08-05 10:03:16 -0700

    Add files via upload
    second commit
    snake move
    ramdom apples are generated
    snake cannot eat the apples

--- snake.js
+++ snake.js
@@ -2,7 +2,7 @@ var _canvas = document.getElementById('canvas');
 var _context = _canvas.getContext("2d");
 var snake_w = 10;
 var snake_h = 10;
-
+var dir = "right";
 
 function drawSnake(x,y){
 _context.fillStyle= "white";
@@ -22,19 +22,74 @@ for(var i = len-1;i>=0;i--){
     })
 }
 
+document.addEventListener("keydown",dirControl)
+
+function dirControl(e)
+{
+    if(e.keyCode==37){
+        dir="left";
+    }
+    else if(e.keyCode==38){
+        dir="up";
+    }
+    else if(e.keyCode==39){
+        dir="right";
+    }
+    else if(e.keyCode==40){
+        dir="down";
+    }
+   
+}
+//create apples
+
+    var apple = {
+        x:Math.floor(Math.random() * 49) + 1,
+        y:Math.floor(Math.random() * 49) + 1
+    }
+
+//draw apple
+function drawApple(x,y){
+    _context.fillStyle= "red";
+    _context.fillRect(x*snake_w,y*snake_h,snake_w, snake_h)
+    _context.fillStyle = "black";
+    _context.strokeRect(x*snake_w,y*snake_h,snake_w, snake_h)
+    
+}
+
 function draw(){
     _context.clearRect(0,0,_canvas.width,_canvas.height)    
     for(i=0;i<len;i++){
     var X = snake[i].x;
     var Y = snake[i].y;
     drawSnake(X,Y)
+
 }
 
+drawApple(apple.x,apple.y);
 
 //snake head
 snakeX = snake[0].x;
 snakeY = snake[0].y;
-snakeX++
+
+
+
+
+
+
+if(dir=="right"){
+    snakeX++
+}
+
+else if(dir=="left"){
+    snakeX--
+}
+else if(dir=="up"){
+    snakeY--
+}
+else if(dir=="down"){
+    snakeY++
+}
+
 snake.pop();
 // new head
 var newHead = {
@@ -44,4 +99,7 @@ var newHead = {
 
 snake.unshift(newHead);
 }
-setInterval(draw,1000);
+setInterval(draw,100);
+
+
+    
